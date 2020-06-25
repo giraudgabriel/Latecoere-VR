2b3d4e92aab73b9296805d235a949c141f9086de
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2020-06-25 20:29:53 -0300

    #CHANGE: Mudança na rota da api.

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use('/api/json',router);
+server.use('/api', router);
 
 let port;
 console.log("❇️ NODE_ENV is", process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
-  port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000;
 } else {
-  port = 3001;
+    port = 3001;
 }
 
 // Start the listener!
 const listener = server.listen(port, () => {
-  console.log("❇️ JSON-server is running on port", listener.address().port);
+    console.log("❇️ JSON-server is running on port", listener.address().port);
 });
