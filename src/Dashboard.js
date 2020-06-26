82c4fc2250538a3dbab850bd599c2a24a75fd653
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2020-06-25 21:21:18 -0300

    #CHANGE: Mudanças nas configurações

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -22,30 +22,32 @@ const Dashboard = () => {
   }
 
   return (
-    <div>
+    <>
       <Menu />
-      <div className="row m-3">
-        <div className="col card">
-          <h3 className="text-center">Cadastro de Montagem</h3>
-          <FormAssembly
-            assembly={selectedAssembly}
-            handleNewAssembly={handleNewAssembly}
-            handleCleanFormAssembly={handleCleanFormAssembly}
-          />
+      <div>
+        <div className="row m-3">
+          <div className="col card">
+            <h3 className="text-center">Cadastro de Montagem</h3>
+            <FormAssembly
+              assembly={selectedAssembly}
+              handleNewAssembly={handleNewAssembly}
+              handleCleanFormAssembly={handleCleanFormAssembly}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <ListAssembly
+              lastAssemblyInserted={lastAssemblyInserted}
+              setSelectedAssembly={setSelectedAssembly}
+            />
+          </div>
+          <div className="col">
+            <ListScore />
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col">
-          <ListAssembly
-            lastAssemblyInserted={lastAssemblyInserted}
-            setSelectedAssembly={setSelectedAssembly}
-          />
-        </div>
-        <div className="col">
-          <ListScore />
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
