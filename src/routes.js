90918bab7723bca65e37fe7ae3367070258f54a7
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2022-03-26 14:27:06 -0300

    #fix: routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import VR from "./pages/VR";
 import Login from "./pages/Login";
@@ -10,62 +10,44 @@ import NotFound from "./pages/NotFound";
 import Ranking from "./pages/Ranking";
 
 export default function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route
-                    exact
-                    path="/"
-                    render={() => (JSON.parse(sessionStorage.getItem('user')) !== null
-                    ? JSON.parse(sessionStorage.getItem('user'))
-                        ?.isAdmin
-                            ? (<Redirect to="/dashboard"/>)
-                            : (<Redirect to="/assembly"/>)
-                        : (<Login/>))}/>
-                <Route
-                    exact
-                    path="/register"
-                    render={() => (JSON.parse(sessionStorage.getItem('user'))
-                    ? JSON.parse(sessionStorage.getItem('user')).isAdmin
-                        ? (<Redirect to="/dashboard"/>)
-                        : (<Redirect to="/assembly"/>)
-                    : (<Register/>))}/>
-                <Route
-                    exact
-                    path="/users"
-                    render={() => (JSON.parse(sessionStorage.getItem('user'))
-                    ?.isAdmin
-                        ? (<Users/>)
-                        : (<Login/>))}/>
-                <Route
-                    exact
-                    path="/dashboard"
-                    render={() => (JSON.parse(sessionStorage.getItem('user'))
-                    ?.isAdmin
-                        ? (<Dashboard/>)
-                        : (<Login/>))}/>
-                <Route
-                    exact
-                    path="/assembly"
-                    render={() => (JSON.parse(sessionStorage.getItem('user'))
-                    ? (<Assembly/>)
-                    : (<Login/>))}/>
-                <Route
-                    exact
-                    path="/vr"
-                    render={() => (JSON.parse(sessionStorage.getItem('user'))
-                    ? (<VR/>)
-                    : (<Login/>))}/>
-                <Route
-                    exact
-                    path="/ranking"
-                    render={() => (JSON.parse(sessionStorage.getItem('user'))
-                    ? (<Ranking/>)
-                    : (<Login/>))}/>
-                <Route
-                    component={NotFound}/>
+  const user = JSON.parse(sessionStorage.getItem("user"));
+  const isAdmin = user && user.isAdmin;
 
-            </Switch>
-        </BrowserRouter>
-    );
+  const goBackToLogin = (route, verifyAdmin) => {
+    if (!user || (verifyAdmin && !isAdmin)) {
+      return <Redirect to="/" />;
+    }
+    return route;
+  };
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Login />} />
+        <Route exact path="/register" render={() => <Register />} />
+        <Route
+          exact
+          path="/users"
+          render={() => goBackToLogin(<Users />, true)}
+        />
+        <Route
+          exact
+          path="/dashboard"
+          render={() => goBackToLogin(<Dashboard />, true)}
+        />
+        <Route
+          exact
+          path="/assembly"
+          render={() => goBackToLogin(<Assembly />, false)}
+        />
+        <Route exact path="/vr" render={() => goBackToLogin(<VR />, false)} />
+        <Route
+          exact
+          path="/ranking"
+          render={() => goBackToLogin(<Ranking />, false)}
+        />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
